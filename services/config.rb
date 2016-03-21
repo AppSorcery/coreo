d09537636cd8d164e30725be044df585d03ee1c9
Adam Taylor <adam@appsorcery.com>
2016-03-20 21:43:34 -0700

    removes services, replaces default vars

--- services/config.rb
+++
@@ -1,9 +0,0 @@
-
-coreo_aws_vpc_routetable "${PRIVATE_ROUTE_NAME}" do
-  action :find_or_create
-  vpc "${VPC_NAME}"
-  number_of_tables 3
-  routes [
-             { :from => "0.0.0.0/0", :to => "STACK::coreo_aws_ec2_autoscaling.${VPC_NAME}.asg_name", :type => :asg }
-        ]
-end
